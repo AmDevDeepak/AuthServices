c769d735e8fdaf868aad44a4c7fa84fe8724835c
Deepak Chauhan <am.deepakkumarchauhan@gmail.com>
2024-07-03 16:31:24 +0530

    Added validation error class

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -14,11 +14,11 @@ const create = async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({
-      message: "An error occurred while creating a user.",
+    return res.status(error.statusCode).json({
+      message: error.message,
       data: {},
       success: false,
-      err: error,
+      err: error.explanation,
     });
   }
 };
